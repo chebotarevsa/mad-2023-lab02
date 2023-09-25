61b800aa7484c861caaa986591ecc27210657211
Даниил Виноходов <strelokst.strelokst@yandex.ru>
2023-09-25 12:26:00 +0300

    Теперь изображение 300 на 300

--- app/src/main/java/com/example/myapplication/MainActivity3.kt
+++ app/src/main/java/com/example/myapplication/MainActivity3.kt
@@ -1,8 +1,5 @@
 package com.example.myapplication
-
-import android.graphics.drawable.Drawable
 import android.os.Bundle
-import android.util.Log
 import android.view.Gravity
 import android.util.TypedValue
 import android.widget.ImageView
@@ -11,27 +8,28 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity3 : AppCompatActivity() {
-    private lateinit var iView: ImageView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val maLay = LinearLayout(this)
-        maLay.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+        maLay.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
             LinearLayout.LayoutParams.MATCH_PARENT
         )
         maLay.orientation = LinearLayout.VERTICAL
 
         val tView = TextView(this)
         tView.text = getString(R.string.HelloWorld)
-        tView.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
-            0, 1.0f
+        tView.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            0,
+            1.0f
         )
         tView.gravity = Gravity.CENTER
         val tSize = resources.getDimension(R.dimen.text_size)
         tView.setTextSize(TypedValue.COMPLEX_UNIT_PX, tSize)
 
-        iView = ImageView(this)
+        val iView = ImageView(this)
         iView.layoutParams = LinearLayout.LayoutParams(
             resources.getDimensionPixelSize(R.dimen.image_width),
             resources.getDimensionPixelSize(R.dimen.image_height)
@@ -44,4 +42,5 @@ class MainActivity3 : AppCompatActivity() {
         maLay.addView(iView)
         setContentView(maLay)
     }
+
 }
